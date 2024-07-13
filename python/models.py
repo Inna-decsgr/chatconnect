feebd19826dfdceff2a4046f7efa115838550c4e
Inna-decsgr <anjgkwl27@gmail.com>
2024-07-13 17:40:07 +0900

    Define new model class for database table

--- python/models.py
+++ python/models.py
@@ -1,4 +1,6 @@
 from python import db
+from datetime import datetime, timezone
+
 
 # User 모델 정의
 class User(db.Model):
@@ -11,4 +13,38 @@ class User(db.Model):
         return {
             'user_id': self.user_id,
             'username': self.username
+        }
+    
+# Messages 모델 정의
+class Messages(db.Model):
+    __tablename__ = 'Messages'  # 여기서 'messages'로 설정
+    message_id = db.Column(db.Integer, primary_key=True)
+    chat_id = db.Column(db.String(36), nullable=False)
+    sender_id = db.Column(db.Integer, nullable=False)
+    receiver_id = db.Column(db.Integer, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+    def to_dict(self):
+        return {
+            'chat_id': self.chat_id,
+            'sender_id': self.sender_id,
+            'receiver_id': self.receiver_id,
+            'text': self.text,
+            'created_at': self.created_at.isoformat()
+        }
+    
+
+# Chat 모델 정의
+class Chats(db.Model):
+    __tablename__ = 'Chats'  # 테이블 이름
+    chat_id = db.Column(db.String(36), primary_key=True)
+    user1_id = db.Column(db.Integer, nullable=False)
+    user2_id = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            'chat_id': self.chat_id,
+            'user1_id': self.user1_id,
+            'user2_id': self.user2_id,
         }
