2468d22dc6bce6671e1eea8b1c6a5b8ac3125781
Inna-decsgr <anjgkwl27@gmail.com>
2025-01-26 01:21:10 +0900

    feat: Add main navigation bar with home and chatting list buttons

---
+++ src/components/MainBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-center py-5">
+    <div class="text-xl">
+      <button class="mb-[20px]" @click="gotoHome">
+        <i class="fa-solid fa-user"></i>
+      </button><br/>
+      <button @click="gotoChattingList">
+        <i class="fa-brands fa-rocketchat"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    gotoHome() {
+      this.$router.push('/mainchat')
+    },
+    gotoChattingList() {
+      this.$router.push('/chattinglist')
+    }
+  }
+}
+</script>
