54bf20e9c26ecfd834aa6f618ac2ed90ecbe1c90
Inna-decsgr <anjgkwl27@gmail.com>
2025-01-31 15:48:14 +0900

    migration: Apply empty migration

---
+++ migrations/versions/9e2c57c96c88_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 9e2c57c96c88
+Revises: a45568d25e1f
+Create Date: 2025-01-31 14:26:16.444933
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+# revision identifiers, used by Alembic.
+revision = '9e2c57c96c88'
+down_revision = 'a45568d25e1f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
