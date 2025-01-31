85bdbbc22034d4fe3523e2770c49b47054032bcf
Inna-decsgr <anjgkwl27@gmail.com>
2025-01-31 15:49:05 +0900

    migration: Manually sync existing tables

---
+++ migrations/versions/a45568d25e1f_manually_sync_existing_tables.py
@@ -0,0 +1,24 @@
+"""Manually sync existing tables
+
+Revision ID: a45568d25e1f
+Revises: 
+Create Date: 2025-01-31 14:25:02.287981
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a45568d25e1f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
