1dc2bb14db28a49a48e6df63ad7ca4836dace85e
Inna-decsgr <anjgkwl27@gmail.com>
2025-02-03 16:30:24 +0900

    Update HomeView and add project logo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,18 @@
 <template>
-  <div class="home text-center">
-    <p class="fw-bold fs-1">ChattConnect</p>
-    <p class="fs-6">소소한 일상을 공유해요!</p>
-    <div class="btn-group w-100" role="group">
-      <button type="button" class="btn btn-primary fw-bold" @click="gotoregister">회원가입</button>
-      <button type="button" class="btn btn-primary fw-bold" @click="gotologin">로그인</button>
+  <div class="flex flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[60%] text-center">
+    <img src="/images/chatlogo.png" alt="챗커넥트 로고" class="w-[230px] mx-auto">
+    <div class="mt-4">
+      <UserLogin />
     </div>
   </div>
 </template>
 
 <script>
-import { useRouter } from 'vue-router';
+import UserLogin from '../components/UserLogin.vue'
+
 export default {
-  setup() {
-    const router = useRouter();
-    return {router}
+  components: {
+    UserLogin
   },
-  methods: {
-    gotoregister() {
-      this.router.push('/register');
-    },
-    gotologin() {
-      this.router.push('/login');
-    }
-  }
 }
 </script>
-
-<style>
-.home {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-</style>
