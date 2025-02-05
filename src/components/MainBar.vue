63d8e6074338f17362a68af002f0450ca2dc26e3
Inna-decsgr <anjgkwl27@gmail.com>
2025-02-05 14:08:09 +0900

    Feat: Highlight active icons in MainBar based on route

--- src/components/MainBar.vue
+++ src/components/MainBar.vue
@@ -2,10 +2,10 @@
   <div class="text-center py-5">
     <div class="text-xl">
       <button class="mb-[20px]" @click="gotoHome">
-        <i class="fa-solid fa-user"></i>
+        <i class="fa-solid fa-user" :class="{'text-gray-400': $route.path !== '/mainchat'}"></i>
       </button><br/>
       <button @click="gotoChattingList">
-        <i class="fa-brands fa-rocketchat"></i>
+        <i class="fa-brands fa-rocketchat" :class="{'text-gray-400' : $route.path !== '/chattinglist'}"></i>
       </button>
     </div>
   </div>
