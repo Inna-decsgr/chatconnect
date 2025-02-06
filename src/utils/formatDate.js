bf554e759dca02b0cba65285cd89a7f07b56c089
Inna-decsgr <anjgkwl27@gmail.com>
2025-02-06 14:36:40 +0900

    Refacotr: Adjust formatDatetime function to consider Korea time difference and improve date comparison

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,29 +1,33 @@
 export function formatDatetime(datetime) {
-  const date = new Date(datetime);  // 주어진 날짜 UTC
+  const date = new Date(datetime); // 주어진 날짜 (UTC)
   const now = new Date();
 
+  // 날짜 비교를 위해 한국 시간으로 변환
+  const koreaDate = new Date(date.getTime() +  60 * 60 * 1000);
+  const koreaNow = new Date(now.getTime() + 60 * 60 * 1000);
 
-  // 일리초 단위 차이 계산(24시간 단위 변환)
-  const diffTime = now.getTime() - date.getTime(); // 밀리초 차이
-  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)); // 하루 단위로 계산
-
-
-  // 하루가 지나지 않았다면 오전/오후 시:분
-  if (diffDays === 0) {
-    const options = { hour: 'numeric', minute: 'numeric', hour12: true };
-    return date.toLocaleString('ko-KR', options);
-  }
-
-  // 하루가 지났다면 '어제'
-  if (diffDays === 1) {
+  // 날짜가 다르면 "어제"로 표시
+  if (
+    koreaNow.getFullYear() === koreaDate.getFullYear() &&
+    koreaNow.getMonth() === koreaDate.getMonth() &&
+    koreaNow.getDate() - koreaDate.getDate() === 1
+  ) {
     return "어제";
   }
 
+  // 같은 날이면 오전/오후 시:분 표시
+  if (
+    koreaNow.getFullYear() === koreaDate.getFullYear() &&
+    koreaNow.getMonth() === koreaDate.getMonth() &&
+    koreaNow.getDate() === koreaDate.getDate()
+  ) {
+    const options = { hour: "numeric", minute: "numeric", hour12: true };
+    return date.toLocaleString("ko-KR", options);
+  }
+
   // 이틀 이상 지났으면 YYYY-MM-DD 형식으로 표시
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // 두 자리로 변환
-  const day = date.getDate().toString().padStart(2, "0"); // 두 자리로 변환
+  const year = koreaDate.getFullYear();
+  const month = (koreaDate.getMonth() + 1).toString().padStart(2, "0");
+  const day = koreaDate.getDate().toString().padStart(2, "0");
   return `${year}-${month}-${day}`;
-
 }
-
