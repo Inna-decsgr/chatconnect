2c1156baa1a078b6c3ca802384b934aa70c93f91
Inna-decsgr <anjgkwl27@gmail.com>
2025-02-07 14:38:59 +0900

    Remove old logo and add new logo with Homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[60%] text-center">
-    <img src="/images/chatlogo.png" alt="챗커넥트 로고" class="w-[230px] mx-auto">
+    <img src="/images/chatlog.png" alt="챗커넥트 로고" class="w-[300px] mx-auto">
     <div class="mt-4">
       <UserLogin />
     </div>
