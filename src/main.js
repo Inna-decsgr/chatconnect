6618474348d0327ddc403c73b9b136cf5808a5d6
Inna-decsgr <anjgkwl27@gmail.com>
2025-02-12 19:08:03 +0900

    Enabled global access to SocketIO in Vue

--- src/main.js
+++ src/main.js
@@ -5,4 +5,10 @@ import store from '../store.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-createApp(App).use(store).use(router).mount('#app')
+// ✅ 소켓 가져오기
+import socket from './utils/socket';
+
+const app = createApp(App);
+app.config.globalProperties.$socket = socket; // Vue 전역에서 사용 가능하도록 설정
+
+app.use(store).use(router).mount('#app');
