39fd6429805e586b91cf61be7883cb998d3d95c0
Inna-decsgr <anjgkwl27@gmail.com>
2025-02-14 15:14:15 +0900

    Add annotations with supplementary explanations

--- python/__init__.py
+++ python/__init__.py
@@ -7,24 +7,27 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 
-load_dotenv() 
+load_dotenv()  # .env 파일 로드해서 환경 변수 사용할 수 있도록 설정
 
 
-app = Flask(__name__)   
-CORS(app, resources={r"/*": {"origins": "*"}})  
+app = Flask(__name__)    # Flask 어플리케이션 인스턴스 생성
+CORS(app, resources={r"/*": {"origins": "*"}})   # 모든 경로와 모든 출처 허용
 
 
+
+# 데이터베이스 URL 설정(MYSQL 연결)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysqlnote@localhost/chat'
+# JWT 비밀 키 설정 (토큰 생성 시 사용)
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY') 
 
-db = SQLAlchemy(app) 
-bcrypt = Bcrypt(app)  
-jwt = JWTManager(app) 
+db = SQLAlchemy(app)  # SQLAlchemy 데이터베이스 객체 생성
+bcrypt = Bcrypt(app)   # Bcrypt 객체 생성(비밀번호 해시용)
+jwt = JWTManager(app)  # Bcrypt 객체 생성(비밀번호 해시용)
 
 # 🔥 Flask-SocketIO 설정
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 
-# 📌 `routes.py`에서 `app`을 가져올 것이므로, import는 맨 아래에서 실행!
+# `routes.py`에서 `app`을 가져올 것이므로, import는 맨 아래에서 실행!
 from python import routes
--- src/router/index.js
+++ src/router/index.js
@@ -40,14 +40,17 @@ const router = createRouter({
 });
 
 
+// 인증되지 않은 사용자가 해당 경로에 접근하려고하면 막기
 router.beforeEach((to, from, next) => {
   const isAuthenticated = store.state.user !== null && Object.keys(store.state.user).length > 0;
+  // 현재 로그인한 사용자가 null이 아니거나(사용자가 있거나) 사용자의 길이가 0보다 클다면 isAuthenticated는 true. 사용자가 없다면 false
 
-
+  // 인증이 필요한 라우트에 접근할 때 사용자 인증이 되지 않았다면
   if (to.matched.some(record => record.meta.requiresAuth) && !isAuthenticated) {
+    // meta.requiresAuth가 true(인증이 필요한 경로인데)인데 (&&) 인증된 사용자가 없다면 => 사용자가 없다면 isAuthenticated가 false인데 !한번 더 붙었으니까 true. true&&true라서 next('/')가 실행
     next('/'); 
   } else {
-    next();  
+    next();  // 인증이 된 상태라면 다음으로 진행
   }
 });
 
